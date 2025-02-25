a060073d9d9b0b71b02a457cd300b873882fb914
Eka <eka.ebong99@gmail.com>
2025-02-25 16:23:46 -0500

    Final Edit
    Final changes and graphs made

--- Assignment1.py
+++ Assignment1.py
@@ -15,7 +15,7 @@ Assume only one explanatory variable is explaining the output
 Learning rate: Explore between 0.01 and 0.1
 Theta_initial = 0
 """
-#TODO Implement cost function
+#Implement cost function
 def cost_function(theta, X, y):
     m = len(y)
     predictions = X.dot(theta)
@@ -23,7 +23,7 @@ def cost_function(theta, X, y):
     return cost
 
 
-#TODO Implement gradient Descent
+#Implement gradient Descent
 def gradient_descent(X, y, learning_rate, iterations, theta):
     m = len(y)
     cost_history = []
@@ -34,11 +34,11 @@ def gradient_descent(X, y, learning_rate, iterations, theta):
         cost_history.append(cost_function(theta,X,y))
     return theta, cost_history
 
-#TODO Function to run 3 times
+#Function to run 3 times
 variables = ['X1', 'X2', 'X3']
 learning_rate_test = [ 0.01, 0.03, 0.05, 0.07, 0.1]
 
-#plot each regression as it happens
+#Plot Exploration of Different Learning Rates for each variable
 for feature in range(len(variables)):
     plt.figure()
     print(variables[feature])
@@ -49,8 +49,6 @@ for feature in range(len(variables)):
         y = np.asarray(data['Y'])
         cost_function(theta, x, y)
         theta, cost_history = gradient_descent(x, y, learning_rate_test[test_rate], 50, theta)
-        #plot cost_history against index
-        #plot y = mx + b
         plt.plot(range(len(cost_history)), cost_history, label=f'Learning Rate: {learning_rate_test[test_rate]}')
         print(learning_rate_test[test_rate])
     plt.xlabel('Iterations')
@@ -60,7 +58,29 @@ for feature in range(len(variables)):
     plt.grid()
     plt.show()
 
-    final_lr = [0.1, 0.03, 0.07]
+final_lr = [0.1, 0.03, 0.07]
+
+#Plot Final Regressions for chosen Alpha values
+for feature in range(len(variables)):
+    plt.figure()
+    theta = np.zeros(2)
+    x1 = np.asarray(data[variables[feature]])
+    x = np.c_[np.ones((100, 1)), x1]
+    y = np.asarray(data['Y'])
+    cost_function(theta, x, y)
+    theta, cost_history = gradient_descent(x, y, final_lr[feature], 50, theta)
+    print("Final Linear Model: y = {:.2f} + {:.2f} * x".format(theta[0], theta[1],))
+
+    #Plot Final Regression
+    plt.plot(x1, (theta[0] + x1*theta[1]), color='red')
+    plt.plot(x1, y, 'o', color='blue', markersize=8)
+    plt.xlabel('X')
+    plt.ylabel('Prediction')
+    plt.title(f'Linear Regression for {variables[feature]}')
+    plt.legend()
+    plt.grid()
+    plt.show()
+
 for feature in range(len(variables)):
     plt.figure()
     print(variables[feature])
@@ -70,49 +90,54 @@ for feature in range(len(variables)):
     y = np.asarray(data['Y'])
     cost_function(theta, x, y)
     theta, cost_history = gradient_descent(x, y, final_lr[feature], 50, theta)
-    #plot cost_history against index
-    #plot y = mx + b
-    print("Final Linear Model: y = {:.2f} + {:.2f} * x".format(theta[0], theta[1],))
-    plt.plot(x1, (theta[0] + x1*theta[1]), color='red')
-    plt.plot(x1, y, 'o', color='blue', markersize=8)
-    plt.xlabel('X')
-    plt.ylabel('Prediction')
+    plt.plot(range(len(cost_history)), cost_history, label=f'Learning Rate: {final_lr[feature]}')
+    plt.xlabel('Iterations')
+    plt.ylabel('Cost Function')
     plt.title(f'Cost Function over Iteration with Feature {variables[feature]}')
     plt.legend()
     plt.grid()
     plt.show()
 
-
-#TODO Plot each Model and loss(cost function)
 """
 Problem 2
 Run a Linear Regression with gradient descent using all 3 variables
 Learning Rate: Explore between 0.01 and 0.1 (your choice)
 """
 
-#TODO implement function with 3 variables
+#Implement function with 3 variables
 #TODO PLOT Loss
 
 x_all = data.iloc[:, :3]
 x_all = np.c_[np.ones((100,1)), x_all]
-theta = np.zeros(4)
-alpha = 0.01
-theta, cost_history = gradient_descent(x_all, y, alpha, 50, theta)
-# Report final linear model
-print("Final Linear Model: y = {:.2f} + {:.2f} * x1 + {:.2f} * x2 + {:.2f} * x3".format(theta[0], theta[1], theta[2], theta[3]))
+alphas = [0.01, 0.03, 0.05, 0.07, 0.1]
+for a in range(len(alphas)):
+    theta = np.zeros(4)
+    theta, cost_history = gradient_descent(x_all, y, alphas[a], 50, theta)
+    # Report final linear model
+    print("Final Linear Model: y = {:.2f} + {:.2f} * x1 + {:.2f} * x2 + {:.2f} * x3".format(theta[0], theta[1], theta[2], theta[3]))
 
-# Plot loss over iterations
-plt.plot(cost_history)
+    # Plot loss over iterations
+    plt.plot(range(len(cost_history)), cost_history, label=f'Learning Rate: {alphas[a]}')
 plt.xlabel('Iterations')
 plt.ylabel('Loss')
-plt.title(f'Cost Function over Iteration of y')
+plt.title(f'Cost Function over Iteration With 3 Variables')
 plt.grid()
 plt.legend()
 plt.show()
 
 # Predict new values
+theta = np.zeros(4)
+theta, cost_history = gradient_descent(x_all, y, 0.05, 50, theta)
+plt.plot(range(len(cost_history)), cost_history, label=f'Learning Rate: {0.05}')
+plt.xlabel('Iterations')
+plt.ylabel('Loss')
+plt.title(f'Cost Function over Iteration With 3 Variables')
+plt.grid()
+plt.legend()
+plt.show()
+
 new_data = np.array([[1, 1, 1], [2, 0, 4], [3, 2, 1]])
-new_data = (new_data - np.mean(new_data, axis=0)) / np.std(new_data, axis=0)  # Normalize
-new_data = np.hstack((np.ones((new_data.shape[0], 1)), new_data))  # Add intercept term
+new_data = np.c_[np.ones((3,1)), new_data]  # Add intercept term
 predictions = new_data.dot(theta)
 print("Predictions for new data:", predictions)
+print("Final Linear Model: y = {:.2f} + {:.2f} * x1 + {:.2f} * x2 + {:.2f} * x3".format(theta[0], theta[1], theta[2], theta[3]))
